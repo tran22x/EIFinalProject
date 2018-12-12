383d35b604ebd89079235d0855d5fe29562ee8cb
nanako <nanako@n228-156.mtholyoke.edu>
2018-12-12 14:20:13 -0500

    Pseudo code for floor_is_lava game

---
+++ src/Floor_is_lava.java
@@ -0,0 +1,41 @@
+
+
+
+import processing.core.PVector;
+
+public class floor_is_lava {
+	PVector person1;
+	PVector person2;
+	int timer;
+
+	public void resetGame() {
+		//TODO: set the game into initial state--two circle that suggests where the person is supposed to be 
+		
+		//if there is exactly two people on the screen & two people are on the designated points=
+		// call gameStart()
+		
+	}
+	
+	public void gameStart() {
+		//TODO: start the game. choose one person to be the player, and other to be rescued.
+		
+		
+		
+	}
+	
+	public void lavaState() {
+		//TODO: controls which part of the map is lava right now 
+	}
+	
+	public boolean detectOnLava() {
+		//TODO: detect if a person touched the lava or not 
+		return false;
+					
+	}
+	
+	
+	
+	
+	
+}
+
